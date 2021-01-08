75777667b78a4fb91b7d0cab0ccc96c3ba8ad9bf
Babis <75264925+babism-dev@users.noreply.github.com>
2021-01-08 14:29:29 +0100

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+####################################
+#
+# Update and Reboot by Babis.M
+#
+####################################
+sudo apt-get -y update &&
+sudo apt-get -y upgrade &&
+sudo apt-get -y dist-upgrade &&
+sudo apt-get -y autoremove &&
+sudo apt-get -y autoclean &&
+sudo apt-get -y update &&
+echo NEUSTART - @Server neustart &&
+sudo reboot
