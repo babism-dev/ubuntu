853258aaab9f9b0bae757b0ef2de82f2b4305634
Babis <75264925+babism-dev@users.noreply.github.com>
2021-01-11 12:58:39 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,6 +10,6 @@ sudo apt-get -y dist-upgrade &&
 sudo apt-get -y autoremove &&
 sudo apt-get -y autoclean &&
 sudo apt-get -y update &&
-udo timedatectl set-timezone Europe/Zurich
+sudo timedatectl set-timezone Europe/Zurich
 echo NEUSTART - @Server neustart &&
 sudo reboot
